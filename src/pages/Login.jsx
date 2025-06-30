90b06b312afad90e9379f8c67450b58e7cf48e21
Hailex <kshitij.singh.bisht.174sis@gmail.com>
2025-06-30 17:50:45 +0530

    Login/SignUp responsive

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,11 +6,11 @@ const Login = () => {
   return (
     <div className="flex w-full  min-h-screen flex-col ">
       <div className="w-full min-h-screen flex flex-col justify-center items-center">
-        <h1 className="text-3xl mb-14 text-black text-center font-bold text-6xl">
+        <h1 className="md:text-6xl text-3xl md:mb-14 mb-7 text-black text-center font-bold">
           LOGIN TO DASHBOARD
         </h1>
         <button
-          className="bg-white text-black h-20 w-60 text-2xl px-3 py-1 rounded-md border-2 border-black
+          className="bg-white text-black md:h-20 md:w-60 md:text-2xl h-10 w-40 text-lg px-3 py-1 rounded-md border-2 border-black
              transition-all duration-200 ease-in-out 
              hover:scale-105 
              active:scale-95 active:shadow-inner"
